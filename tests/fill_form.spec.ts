fa11fb5b13af8696e5f1bd268040ae52be8517f8
Kevin Alejandro Manzano Torres <manzanokevin5@gmail.com>
2024-12-16 15:17:04 -0500

    test: :white_check_mark: overwrite actor default configuration

--- tests/fill_form.spec.ts
+++ tests/fill_form.spec.ts
@@ -1,12 +1,26 @@
-import { describe, it } from "@serenity-js/playwright-test";
+import { describe, it, test } from "@serenity-js/playwright-test";
+import { actorCalled } from "@serenity-js/core";
+import { BrowseTheWebWithPlaywright } from "@serenity-js/playwright";
 
 import { fillForm, submitForm, uploadImage, validateFormSended } from "./tasks";
+
+test.use({
+  actorCalled: async ({ actorCalled, contextOptions, page }, use) => {
+    await use((name: string) => {
+      return actorCalled(name).whoCan(
+        BrowseTheWebWithPlaywright.usingPage(page, contextOptions)
+      );
+    });
+  },
+});
 
 describe("Practice Form", () => {
   it("should allow the form to be submitted if all the data is correct", async ({
     actor,
     page,
   }) => {
+    actor = actorCalled("Tony");
+
     await actor.attemptsTo(
       fillForm(),
       uploadImage(page),
